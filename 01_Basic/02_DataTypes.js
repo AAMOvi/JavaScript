6523aea8036757195ccf7f7e46fb194aa12d2e16
AAMOvi <aamo.official@gmail.com>
2024-07-04 18:04:48 +0000

    data type learned

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3); we are using node js, not browser; code readability
+let name="Ovi";
+let age=21;
+let isLoggedIn=false;
+//number,bigint,string=>"",boolean=> true/false
+//null=>stand alone value
+//undefined=>not assigned
+//symbol=>unique
+
+//object
+console.table([typeof"Ovi",typeof age,typeof null, typeof undefined]);
