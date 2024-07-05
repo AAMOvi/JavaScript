8d82c7b8adf188ad21bbb830d477d1eada053ad3
AAMOvi <aamo.official@gmail.com>
2024-07-05 06:01:21 +0000

    Operations learned

--- 01_Basic/03_ConversionOperation.js
+++ 01_Basic/03_ConversionOperation.js
@@ -1,17 +1,31 @@
 //let score="33abc";
 let score =null;
-console.log(typeof score);
+// console.log(typeof score);
 let valueInNumber= Number(score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 //"33"=>33
 //"33abc"=>NaN
 //True=>1; false=>0;
 let isLoggedIn= 1;
 let booleanIsLoggedIn= Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1=> true; 0=>false;
 // ""=> false, ovi=>true;
 let Snumber=33;
 let Sstring= String(Snumber);
-console.log(typeof Snumber);
+// console.log(typeof Snumber);
+
+// **********Operations*********
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+let str1= "Hello ";
+let str2= "World";
+let str3= str1+ str2;
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"3");
